e70ed2980ed4f41225bc31e878bde73d5adf86f4
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-02-24 13:14:13 +0800

    Updating formatting on the part where it calls pandoc

--- src/extract.rs
+++ src/extract.rs
@@ -1,14 +1,15 @@
+use crate::models::Link;
+use scraper::{Html, Selector};
 use std::io::Write;
 use std::process::{Command, Stdio};
-use scraper::{Html, Selector};
-use crate::models::Link;
 
 pub fn extract_links(html: &String) -> Vec<Link> {
     let document = Html::parse_document(&html);
     let selector_title = Selector::parse("a.result__a").unwrap();
     let selector_url = Selector::parse("a.result__url").unwrap();
 
-    document.select(&selector_title)
+    document
+        .select(&selector_title)
         .zip(document.select(&selector_url))
         .take(5)
         .map(|(title, url)| Link {
@@ -25,13 +26,19 @@ pub fn extract_page_content(res: &String) -> Result<String, String> {
         .map(|ele| ele.text().collect::<Vec<_>>().join("\n"))
         .collect();
 
-    let cols = std::env::var("COLUMNS").unwrap_or_else(|_| "180".to_string())
-        .parse::<usize>().unwrap_or(180).saturating_sub(5);
+    let cols = std::env::var("COLUMNS")
+        .unwrap_or_else(|_| "180".to_string())
+        .parse::<usize>()
+        .unwrap_or(180)
+        .saturating_sub(5);
 
     Command::new("pandoc")
-        .arg("-f").arg("html")
-        .arg("-t").arg("ansi")
-        .arg("--columns").arg(cols.to_string())
+        .arg("-f")
+        .arg("html")
+        .arg("-t")
+        .arg("ansi")
+        .arg("--columns")
+        .arg(cols.to_string())
         .env("TERM", "xterm-256color")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
@@ -41,6 +48,10 @@ pub fn extract_page_content(res: &String) -> Result<String, String> {
                 stdin.write_all(page.as_bytes()).ok();
             }
             child.wait_with_output()
-        }).map(|out| String::from_utf8_lossy(&out.stdout).to_string())
+        })
+        .map(|out| {
+            String::from_utf8(out.stdout)
+                .unwrap_or(String::from("Error: Could not convert to ansi"))
+        })
         .map_err(|_| String::from("Error: Could not convert to ansi"))
 }
