a018133a68557d7425a8d23ca74f156d69eba95a
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-03-01 10:34:02 +0800

    Making the history display better

--- src/database/connect.rs
+++ src/database/connect.rs
@@ -1,6 +1,7 @@
 use crate::errors::error::MyError;
 use crate::errors::error::MyError::DatabaseError;
 use crate::links::link::Link;
+use chrono::{NaiveDateTime, Utc};
 use once_cell::sync::Lazy;
 use rusqlite::Connection;
 use std::sync::Mutex;
@@ -8,7 +9,7 @@ use std::sync::Mutex;
 static CONNECTION: Lazy<Mutex<Connection>> = Lazy::new(|| {
     let conn = Connection::open("is-fast.db").expect("Failed to open database");
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS history (title TEXT, url TEXT, time DATE)",
+        "CREATE TABLE IF NOT EXISTS history (title TEXT, url TEXT, time DATETIME)",
         [],
     ).expect("Failed to create table");
     Mutex::new(conn)
@@ -26,7 +27,7 @@ pub fn add_history(link: &Link) -> Result<(), MyError> {
 
 pub fn show_history() -> Result<String, MyError> {
     let conn = CONNECTION.lock().unwrap();
-    let mut stmt = conn.prepare("SELECT title, url, time FROM history ORDER BY time DESC")?;
+    let mut stmt = conn.prepare("SELECT title, url, time FROM history ORDER BY time DESC LIMIT 20")?;
     let history = stmt.query_map([], |row| {
         Ok(
             History {
@@ -38,11 +39,47 @@ pub fn show_history() -> Result<String, MyError> {
     }).map_err(|e| DatabaseError(e))?;
     let mut result = String::new();
 
+    let mut count = 1;
     for history_item_result in history {
         let history_item = history_item_result?;
-        result.push_str(&format!("{} ({})\n", history_item.title, history_item.url));
+        result.insert_str(0, &format!("{} | {} | {} ({})\n", count, date_to_display(history_item.time), clip_if_needed(history_item.title, 100), clip_if_needed(history_item.url, 30)));
+        count += 1;
     }
     Ok(result)
+}
+
+fn clip_if_needed(text: String, max_length: usize) -> String {
+    if text.len() > max_length {
+        return format!("{}...", &text[0..max_length-3]);
+    }
+    text.to_string()
+}
+
+fn date_to_display(date: String) -> String {
+    let now = Utc::now();
+    NaiveDateTime::parse_from_str(&*date, "%Y-%m-%d %H:%M:%S")
+        .map(|parsed_datetime| {parsed_datetime.and_utc()})
+        .map(|datetime_utc| now.signed_duration_since(datetime_utc))
+        .map(|duration| {
+            if duration.num_weeks() > 0 {
+                return format!("{} weeks", duration.num_weeks());
+            }
+            if duration.num_days() > 0 {
+                return format!("{} days", duration.num_days());
+
+            }
+            if duration.num_hours() > 0 {
+                return format!("{} hours", duration.num_hours());
+            }
+            if duration.num_minutes() > 0 {
+                return format!("{} minutes", duration.num_minutes());
+            }
+            if duration.num_seconds() > 0 {
+                return format!("{} seconds", duration.num_seconds());
+            }
+            "Date could not be displayed".to_string()
+        })
+        .unwrap_or_else(|_| "Date could not be displayed".to_string())
 }
 
 struct History {
