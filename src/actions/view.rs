4b19ccd49da3f8b8193cc8f82a2501c7d8ed026a
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-03-02 16:25:54 +0800

    Fixing bug with the file flag

--- src/actions/view.rs
+++ src/actions/view.rs
@@ -1,6 +1,5 @@
 use crate::formatting::format::to_display;
 use crate::links::link::Link;
-use crate::scrapers::scrape::scrape;
 use crate::stout::pipe::out_to_std;
 use crate::tui::browser::Browser;
 use ratatui::text::Text;
@@ -9,15 +8,10 @@ use std::fs;
 pub fn run(file: String, url: Option<String>, piped: bool) {
     let url = url.unwrap_or_else(|| file.clone());
     let html = fs::read_to_string(&file).unwrap();
-    let link = Link::new(file, url, move || Ok(html.clone()));
     if piped {
-        out_to_std(
-            scrape(&link.url)
-                .and_then(|html| to_display(&link.url, &html))
-                .unwrap_or_else(|_| Text::from("Failed to convert to text")),
-        );
-
+        out_to_std(to_display(&url, &html.clone()).unwrap_or_else(|_| Text::from("Failed to convert to text")));
         return;
     }
+    let link = Link::new(file, url, move || Ok(html.clone()));
     Browser::new().browse(vec![link]);
 }
